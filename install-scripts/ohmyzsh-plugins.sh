d898758919b6c0fc9f82f2626612be98d2c4a8e7
Tao Xu <tao@jbobos.com>
2021-08-28 23:37:53 +0930

    fix install script for oh-my-zsh plugins

--- install-scripts/ohmyzsh-plugins.sh
+++ install-scripts/ohmyzsh-plugins.sh
@@ -7,9 +7,6 @@
 PLUGINS=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 set -ex \
-  # zsh-autosuggestions
   && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
-  # zsh-syntax-highlighting
   && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
-  # update plugins list
-  && sed -i -e "s/^plugins=.*/plugins=${PLUGINS}/g" ~/.zshrc
+  && sed -i -e "s/^plugins=.*/plugins=(${PLUGINS[*]})/g" ~/.zshrc
