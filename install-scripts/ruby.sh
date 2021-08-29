bd62eccd686d9219164385312c5497028c479b68
Tao Xu <tao@jbobos.com>
2021-08-29 19:06:24 +0930

    refactoring ruby

--- install-scripts/ruby.sh
+++ install-scripts/ruby.sh
@@ -2,38 +2,40 @@
 #
 # install rbenv and ruby
 #  - install rbenv
-#  - install ruby (default = 2.7.4)
+#  - install ruby
 #
-# rbenv
-RBENV=$(cat <<'EOF'
+RBENV_ZSHRC=$(cat <<'EOF'
 # rbenv
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 EOF
 )
 
-set -ex \
-  && git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
-  && echo $'\n'"$RBENV" >> ~/.zshrc
-
-# ruby-build
-# dependencies - https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-set -ex \
-  && sudo apt-get update \
-  && sudo apt-get install --yes autoconf bison build-essential libssl-dev libyaml-dev \
-    libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev \
-  && sudo apt-get clean
-
-# ruby-build
-# as an rbenv plugin - https://github.com/rbenv/ruby-build
-set -ex \
-  && mkdir -p ~/.rbenv/plugins \
-  && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-# ruby
-mapfile -t RUBY_VERSIONS < <(~/.rbenv/bin/rbenv install --list-all)
-if [[ -n ${RUBY} ]] && [[ ${RUBY_VERSIONS[*]} =~ ${RUBY} ]]; then
+if [[ -n ${RUBY} ]]; then
+  # rbenv
   set -ex \
-    && ~/.rbenv/bin/rbenv install ${RUBY} \
-    && ~/.rbenv/bin/rbenv local ${RUBY}
+    && git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
+    && echo $'\n'"$RBENV_ZSHRC" >> ~/.zshrc
+
+  # ruby-build
+  # dependencies - https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
+  set -ex \
+    && sudo apt-get update \
+    && sudo apt-get install --yes autoconf bison build-essential libssl-dev libyaml-dev \
+      libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev \
+    && sudo apt-get clean
+
+  # ruby-build
+  # as an rbenv plugin - https://github.com/rbenv/ruby-build
+  set -ex \
+    && mkdir -p ~/.rbenv/plugins \
+    && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+  # ruby
+  mapfile -t RUBY_VERSIONS < <(~/.rbenv/bin/rbenv install --list-all)
+  if [[ ${RUBY_VERSIONS[*]} =~ ${RUBY} ]]; then
+    set -ex \
+      && ~/.rbenv/bin/rbenv install ${RUBY} \
+      && ~/.rbenv/bin/rbenv local ${RUBY}
+  fi
 fi
