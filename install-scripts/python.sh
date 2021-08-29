ce43f6d1ba0ba65ac9af2610c636ac2b2fdaf0d8
Tao Xu <tao@jbobos.com>
2021-08-29 20:23:58 +0930

    refactoring python

--- install-scripts/python.sh
+++ install-scripts/python.sh
@@ -2,10 +2,9 @@
 #
 # install pyenv and python
 #  - install pyenv
-#  - install python (default = 3.9.6)
+#  - install python
 #
-# pyenv
-PYENV=$(cat <<'EOF'
+PYENV_ZSHRC=$(cat <<'EOF'
 # pyenv
 export PATH="$HOME/.pyenv/bin:${PATH}"
 export PATH="$HOME/.pyenv/shims:${PATH}"
@@ -13,23 +12,29 @@ eval "$(pyenv init -)"
 EOF
 )
 
-set -ex \
-  &&  git clone https://github.com/pyenv/pyenv.git ~/.pyenv \
-  && echo $'\n'"$PYENV" >> ~/.zshrc
-
-# python-build
-# dependencies - https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-set -ex \
-  && sudo apt-get update \
-  && sudo apt-get install –-no-install-recommends --yes make build-essential libssl-dev \
-    zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev \
-    xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-  && sudo apt-get clean
-
-# python
-mapfile -t PYTHON_VERSIONS < <(~/.pyenv/bin/pyenv install --list)
-if [[ -n ${PYTHON} ]] && [[ ${PYTHON_VERSIONS[*]} =~ ${PYTHON} ]]; then
+if [[ -n ${PYTHON} ]]; then
+  # pyenv
   set -ex \
-    && ~/.pyenv/bin/pyenv install ${PYTHON} \
-    && ~/.pyenv/bin/pyenv local ${PYTHON}
+    &&  git clone https://github.com/pyenv/pyenv.git ~/.pyenv \
+    && echo $'\n'"$PYENV_ZSHRC" >> ~/.zshrc
+
+  # python-build
+  # dependencies - https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+  set -ex \
+    && sudo apt-get update \
+    && sudo apt-get install –-no-install-recommends --yes make build-essential libssl-dev \
+      zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev \
+      xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+    && sudo apt-get clean
+
+  # available versions
+  mapfile -t PYTHON_VERSIONS < <(~/.pyenv/bin/pyenv install --list)
+  # remove the words "Available versions:" from list
+  unset PYTHON_VERSIONS[0]
+  # python
+  if [[ ${PYTHON_VERSIONS[*]} =~ ${PYTHON} ]]; then
+    set -ex \
+      && ~/.pyenv/bin/pyenv install ${PYTHON} \
+      && ~/.pyenv/bin/pyenv local ${PYTHON}
+  fi
 fi
