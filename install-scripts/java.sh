db3512e01e49fffcf982d996feb9804631036176
Tao Xu <tao@jbobos.com>
2021-08-30 23:32:20 +0930

    install script for jenv and java

---
+++ install-scripts/java.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# install jenv and java
+#  - install jenv
+#  - install java and maven
+#
+JENV_ZSHRC=$(cat <<'EOF'
+# jenv
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+EOF
+)
+
+if [[ -n ${JAVA} ]]; then
+  # jenv
+  set -ex \
+    && git clone https://github.com/jenv/jenv.git ~/.jenv \
+    && echo $'\n'"$JENV_ZSHRC" >> ~/.zshrc
+
+  # initialize jenv
+  export PATH="$HOME/.jenv/bin:$PATH"
+  eval "$(jenv init -)"
+
+  # make sure JAVA_HOME is set, maven is enabled
+  set -ex \
+    && jenv enable-plugin export \
+    && jenv enable-plugin maven
+
+  # install java and maven
+  set -ex \
+    && sudo apt-get install --no-install-recommends --yes openjdk-${JAVA}-jdk maven \
+    && jenv add /usr/lib/jvm/java-${JAVA}-openjdk-amd64/ \
+    && jenv versions | grep openjdk64 | xargs jenv local \
+    && jenv doctor \
+    && mvn --version
+fi
