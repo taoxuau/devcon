e9c42094f922d43e2e59c23d9b17795e2792d443
Tao Xu <tao@jbobos.com>
2021-08-31 21:30:18 +0930

    install multiple ruby

--- install-scripts/ruby.sh
+++ install-scripts/ruby.sh
@@ -12,10 +12,14 @@ EOF
 )
 
 if [[ -n ${RUBY} ]]; then
-  # rbenv
+  # install rbenv
   set -ex \
     && git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
     && echo $'\n'"$RBENV_ZSHRC" >> ~/.zshrc
+
+  # initialize rbenv
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
 
   # ruby-build
   # dependencies - https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
@@ -31,11 +35,23 @@ if [[ -n ${RUBY} ]]; then
     && mkdir -p ~/.rbenv/plugins \
     && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-  # ruby
-  mapfile -t RUBY_VERSIONS < <(~/.rbenv/bin/rbenv install --list-all)
-  if [[ ${RUBY_VERSIONS[*]} =~ ${RUBY} ]]; then
+  # available versions
+  mapfile -t RUBY_VERSIONS < <(rbenv install --list-all)
+
+  # install multiple ruby, e.g. RUBY="2.7.4, 3.0.2"
+  IFS=, read -ra TO_INSTALL <<<${RUBY}
+  for SINGLE_RUBY in ${TO_INSTALL[@]}
+  do
+    if [[ ${RUBY_VERSIONS[*]} =~ ${SINGLE_RUBY} ]]; then
+      set -ex && rbenv install ${SINGLE_RUBY}
+    fi
+  done
+
+  # configure rbenv - use the first version found
+  INSTALLED_VERSIONS=($(rbenv versions))
+  if [[ -n ${INSTALLED_VERSIONS[0]} ]]; then
     set -ex \
-      && ~/.rbenv/bin/rbenv install ${RUBY} \
-      && ~/.rbenv/bin/rbenv local ${RUBY}
+      && rbenv local ${INSTALLED_VERSIONS[0]} \
+      && rbenv versions
   fi
 fi
