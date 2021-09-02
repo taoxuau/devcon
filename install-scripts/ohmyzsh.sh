04cb5e87424a30a9c0f826e98e593c77adaae022
Tao Xu <tao@jbobos.com>
2021-09-02 10:08:52 +0930

    add personal local path

--- install-scripts/ohmyzsh.sh
+++ install-scripts/ohmyzsh.sh
@@ -7,8 +7,15 @@
 #
 PLUGINS=(git zsh-autosuggestions zsh-syntax-highlighting)
 
+LOCAL_PATH=$(cat <<'EOF'
+# local path - personal scripts, binstubs ...
+export PATH="$HOME/.local/bin:$PATH"
+EOF
+)
+
 set -ex \
   && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
   && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
   && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
-  && sed -i -e "s/^plugins=.*/plugins=(${PLUGINS[*]})/g" ~/.zshrc
+  && sed -i -e "s/^plugins=.*/plugins=(${PLUGINS[*]})/g" ~/.zshrc \
+  && echo $'\n'"$LOCAL_PATH" >> ~/.zshrc
